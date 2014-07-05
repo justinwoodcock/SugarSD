10e75fb93e8a01e5d11c9a8973fa48f108d8e9a2
Justin Woodcock <jwoodcock@undergroundelephant.com>
2014-07-05 10:18:26 -0700

    disable map scroll wheel

--- app/js/main/controller.main.js
+++ app/js/main/controller.main.js
@@ -2,8 +2,11 @@
 
 uiBase.controller('MainController', ['$scope', 'parallaxHelper',
     function($scope, parallaxHelper) {
-        L.mapbox.map('map', 'justinwoodcock.il70lk8f', {
+        var map = L.mapbox.map('map', 'justinwoodcock.il70lk8f', {
             shareControl: false
-        })
+        });
+        // Disable drag and zoom handlers.
+        map.touchZoom.disable();
+        map.scrollWheelZoom.disable();
     }
 ]);
