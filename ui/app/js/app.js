d0454222812c8491bce48d877b91f5eaae2c48b0
Justin Woodcock <justin@digitaluprisingdesign.com>
2014-09-17 21:10:34 -0700

    move ui to subdirectory

---
+++ ui/app/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var uiBase = angular.module('uiBase', [
+    'ui.router',
+    'ui.bootstrap',
+    'duParallax'
+]);
+
+uiBase.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    $stateProvider.state('main', {
+        url: '/',
+        views: {
+            content: {
+                templateUrl: 'views/main/partial.main.html',
+                controller: 'MainController'
+            }
+        }
+    }).state('info', {
+        url: '/info',
+        views: {
+            content: {
+                templateUrl: '/views/info/partial.info.html',
+                controller: 'InfoController'
+            }
+        }
+    });
+
+    $urlRouterProvider.otherwise('/');
+});
