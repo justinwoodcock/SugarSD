a8cb39640d05dce5c2cee23b767fc9ee734ab944
Justin Woodcock <justin@digitaluprisingdesign.com>
2014-10-31 09:35:35 -0700

    ui/app dir removed from dist branch

--- ui/app/components/_template/template.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-uiBase.controller('TemplateController', ['$scope',
-    function($scope) {
-        // you're welcome for the controller.
-        $scope.test = 'stubbing out test';
-    }
-]);
